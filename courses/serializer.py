65a42bde396e255e311fb74d8990a68461df927c
Michael Zapata <mikeyfriends@hotmail.com>
2020-05-18 23:47:26 +0100

    basic setup

--- courses/serializer.py
+++ courses/serializer.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
-from .models import Course
+from .models import Course # we need to serialize the model data
 
 class CourseSerializer(serializers.ModelSerializer):
     """Serializer for Course object"""
 
+    #
     class Meta:
-        model = Course
-        fields = ('id', 'name', 'language', 'price')
-        read_only_Fields = ('id',)
+        model = Course # name of model
+        fields = ('id', 'name', 'language', 'price') #fields we want to serialize( convert to/from JSON)
+        read_only_Fields = ('id',) #fields that we want to protect
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets #sets of pages that the rest_framework will create for us
+from .models import Course
+from .serializer import CourseSerializer
 
-# Create your views here.
+class CourseView(viewsets.ModelViewSet):
+    queryset = Course.objects.all() # this is the model (dataset), so we need to pull out the data
+    serializer_class = CourseSerializer # Specify which serializer_class to use (show) when this view is accessed/served
+    
