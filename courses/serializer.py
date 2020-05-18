1725481ff96e2a57129a33ccde30326691a69ec4
Michael Zapata <mikeyfriends@hotmail.com>
2020-05-18 23:14:45 +0100

    modified

--- courses/serializer.py
+++ courses/serializer.py
@@ -1,2 +1,10 @@
 from rest_framework import serializers
 from .models import Course
+
+class CourseSerializer(serializers.ModelSerializer):
+    """Serializer for Course object"""
+
+    class Meta:
+        model = Course
+        fields = ('id', 'name', 'language', 'price')
+        read_only_Fields = ('id',)
